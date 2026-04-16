bd2c79c1eb00da922ebe412bdee35ef6a76a30b5
James Hart <speddling@gmail.com>
2026-04-16 19:24:29 -0400

    vault backup: 2026-04-16 19:24:29

---
+++ terraform/bootstrap/main.tf
@@ -0,0 +1,12 @@
+resource "null_resource" "k3s_bootstrap" {
+  # This runs on Monolith via your GitHub Runner
+  provisioner "local-exec" {
+    command = <<EOT
+      curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="server \
+        --write-kubeconfig-mode 644 \
+        --disable local-storage \
+        --etcd-expose-metrics true \
+        --datastore-endpoint='path=/mnt/k8s-etcd'" sh -
+    EOT
+  }
+}
