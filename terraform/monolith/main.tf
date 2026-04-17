e065cb789c484db1520112235f73e09babf451c3
James Hart <speddling@gmail.com>
2026-04-17 01:43:49 -0400

    local-storage

--- terraform/monolith/main.tf
+++ terraform/monolith/main.tf
@@ -4,7 +4,6 @@ resource "null_resource" "k3s_bootstrap" {
     command = <<EOT
       curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="server \
         --write-kubeconfig-mode 644 \
-        --disable local-storage \
         --etcd-expose-metrics true \
         --datastore-endpoint='path=/mnt/k8s-etcd'" sh -
     EOT
